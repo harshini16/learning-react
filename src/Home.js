8af578443a46e3ddff4ec0732913158857e86952
harshini16 <mharshini16@gmail.com>
2023-10-05 17:10:20 +0530

    Updates2

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,57 @@
+import { useState } from "react";
 const Home = () => {
-    const handleClick = (e) => {
-        alert('hello, you have clicked the button!',e);
-    }
+    // let name = "aria";
 
+    /*Click events */
+    // const handleClick = (e) => {
+    //     alert('hello, you have clicked the button!',e);
+    // }
+    /*End of Click events example */
+
+    /* UseState Hook example */
+    /*
+    const [name, setName] = useState('aria');
+    const [age, setAge] = useState(24);
+    const handleClick2 = () => {
+        setName("luca");
+        setAge(29);
+        // console.log(name);
+        // console.log(setName);
+    } 
+    */
+    /* End of UseState Hook example */
+
+    /*Outputting lists/blogs */
+    const [blogs, setBlogs] = useState([
+        {title: 'My New website', body: 'lorem wei j fij ajfom aj', author: 'aemma', id: 1},
+        {title: 'My 2nd website', body: 'loremasbganetij ajfom aj', author: 'watson', id: 2},
+        {title: 'My 3rd website', body: 'lDVGsrjjmdf fij ajfom aj', author: 'dumbledore', id: 3}
+    ]);
+/*
     const handleCLickAgain = (name, e) => {
         console.log('Hello '+name );
     }
+     */
     return ( 
         <div className="Home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>CLICK ME </button>
-            <button onClick={() => handleCLickAgain('harshini')}>CLICK ME again </button>
+            {/* <h2>Homepage</h2>
+            //Click events
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>CLICK ME </button> 
+            <button onClick={handleClick2}>CLICK ME </button>
+            <button onClick={() => handleCLickAgain('harshini')}>CLICK ME again </button> */}
+
+            {/* Outputing links/blogs , props */}
+
+            {blogs.map((blog)=> (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+
+            {/* End of Outputing links/blogs , props */}
+
         </div>
 
     );
