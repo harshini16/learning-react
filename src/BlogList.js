3a3e5f6c0787aab17afb9a7dd1871a9bf32fd64c
harshini16 <mharshini16@gmail.com>
2023-10-06 17:30:31 +0530

    bloglist

--- src/BlogList.js
+++ src/BlogList.js
@@ -12,6 +12,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
